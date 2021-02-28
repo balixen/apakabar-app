3a68334ec19267585ae284ea87277818e23b7c66
balixen <fgedwin@balixen.com>
2021-02-28 15:04:46 +0800

    update script

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+//component - properties
+function Greeting(props) {
+  return (
+    <h1>
+      Halo {props.name} - umurnya {props.age}
+    </h1>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <Greeting name="Bee Bee" age="25" />
+
         <p>
           Apa kabar - Edit <code>src/App.js</code> and save to reload.
         </p>
